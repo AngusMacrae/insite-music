b107494d3f43a0e50dfc1c41af350027b2648e14
Angus Macrae <angusdmacrae@gmail.com>
2020-05-12 10:53:08 +0800

    Built outline of the result page

--- js/script.js
+++ js/script.js
@@ -13,13 +13,12 @@ showInputPageBtn.addEventListener('click', showPage.bind(this, 2));
 submitBtn.addEventListener('click', function (e) {
 
     e.preventDefault();
-    // start loading animation
+    showPage(3);
     // send to a php script which calls python script and returns JSON
 //    makeAJAXrequest("POST", "url.com/blac", showResults, true);
     
     // temp
-    showPage(3);
-
+    setTimeout(showResults, 2500);
 
 });
 
@@ -56,16 +55,15 @@ function showPage(pageNum) {
 
     for (i = 0; i < pages.length; i++) {
         pageContainer.classList.remove("show-page-" + (i + 1))
-        pages[i].classList.remove("active-page");
+//        pages[i].classList.remove("active-page");
     }
     pageContainer.classList.add("show-page-" + (pageNum));
-    pages[pageNum - 1].classList.add("active-page");
+//    pages[pageNum - 1].classList.add("active-page");
 
 }
 
 function showResults(data) {
     
-    // end loading animation
     if (data) {
         
         // fill results page with charts etc constructed from JSON
@@ -74,7 +72,88 @@ function showResults(data) {
         
         //display failure message
         
+        //temp
+        //populate charts with dummy data
+        showPage(4);
+        
     }
 
 }
 
+let resizeId;
+let resizeToggle = true;
+
+window.addEventListener("resize", function () {
+    
+    if (resizeToggle) {
+        pageContainer.classList.toggle("transition");
+    }
+    resizeToggle = false;
+    clearTimeout(resizeId);
+    resizeId = setTimeout(doneResizing, 500);
+    
+});
+
+function doneResizing(){
+    pageContainer.classList.toggle("transition");
+    resizeToggle = true;
+}
+
+Chart.defaults.global.defaultFontColor = 'white';
+var ctx = document.getElementById('chart1').getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
+
+new Chart(document.getElementById("doughnut-chart"), {
+    type: 'doughnut',
+    data: {
+      labels: ["Africa", "Asia", "Europe", "Latin America", "North America"],
+      datasets: [
+        {
+          label: "Population (millions)",
+          backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850"],
+          data: [2478,5267,734,784,433]
+        }
+      ]
+    },
+    options: {
+      title: {
+        display: true,
+        text: 'Predicted world population (millions) in 2050'
+      }
+    }
+});
